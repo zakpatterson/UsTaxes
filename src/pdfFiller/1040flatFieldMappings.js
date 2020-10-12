97b02727bcf8358797ae16c23b6b27ddd04e8da8
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-10-12 00:00:28 -0700

    pdfCDN, only has pdf files :

--- src/pdfFiller/1040flatFieldMappings.js
+++
@@ -1,9 +0,0 @@
-flatFieldMappings = {
-    First               :   6,
-    Last                :   7,
-    SSID                :   8,
-    EmployeeAddress     :   12,
-    ForeignCountry      :   15,
-    ForeignProvidence   :   16,
-    ForeignPostalCode   :   17
-}
--- src/redux/selectors.js
+++
@@ -1,6 +0,0 @@
-export function getFormData(state, id) {
-    return (state["information"][id] ? 
-            state["information"][id] : 
-            {}
-        )
-}
